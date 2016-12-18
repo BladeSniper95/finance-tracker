01b06221edea1e2984a8c874890f6ee36715a3a7
Marcus Moss <mossm7@students.rowan.edu>
2016-12-18 23:18:19 +0000

    add user and stock association and display

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,23 +1,20 @@
 class Stock < ActiveRecord::Base
-    def self.find_by_ticker(ticker_symbol)
+ 
+ has_many :user_stocks
+ has_many :users, through: :user_stocks 
+ 
+def self.find_by_ticker(ticker_symbol)
+  where(ticker: ticker_symbol).first
+end
 
-    where(ticker: ticker_symbol).first
-
-    end
-
-    def self.new_from_lookup(ticker_symbol)
-
-    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-
-    return nil unless looked_up_stock.name
+def self.new_from_lookup(ticker_symbol)
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      return nil unless looked_up_stock.name
 
     new_stock = new(ticker: looked_up_stock.symbol, name: looked_up_stock.name)
-
     new_stock.last_price = new_stock.price
-
     new_stock
-
-    end
+end
 
     def price
 
